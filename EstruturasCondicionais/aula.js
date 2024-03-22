4b85f6d301ca4676caf307092934797e6fe7e138
André Luiz da Silva <andre.java.ads.2022@gmail.com>
2024-03-22 19:34:09 -0300

    Revisao e dicas de boas práticas

--- EstruturasCondicionais/aula.js
+++ EstruturasCondicionais/aula.js
@@ -1,15 +1,8 @@
-const numero = 0;
+const numero = 95;
+const isNumeroEhPar = (numero % 2) === 0;
 
-const numeroEhPar = numero % 2 === 0;
-const numeroPar = numero % 2 == '0'; // 2 iguais ignora o tipo da variavel
-
-
-if(!numeroEhPar){
-    console.log('\nO número não é par');
-}
-else if(numero === 0){
-    console.log('O número é inválido');
-}
-else{
+if(isNumeroEhPar){
     console.log('O número é par');
+}else{
+    console.log('O número não é par');
 }
