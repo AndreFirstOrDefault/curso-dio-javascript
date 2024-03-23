5059cafc9f24f553c3d145c9988f7f3967108d8c
André Luiz da Silva <andre.java.ads.2022@gmail.com>
2024-03-23 16:28:33 -0300

    Objetos - aula 03

--- Objetos/aula01.js
+++ Objetos/aula01.js
@@ -1,14 +1,25 @@
-const andre = {
+const pessoa = {
     nome: 'André Luiz',
-    idade: 35
+    idade: 35,
+
+    // Método, função.
+    descrever: function(){
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+    }
 };
 
-console.log(andre.nome);
-console.log(andre.idade);
-console.log(andre);
+// console.log(andre.nome);
+// console.log(andre.idade);
+// console.log(andre);
 
-andre.altura = 1.69;
+// andre.altura = 1.69;
 
-delete andre.nome;
+// delete andre.nome;
 
-console.log(andre);
+// console.log(andre);
+
+pessoa.descrever = function(){
+    console.log(`Meu nome é ${this.nome}`);
+}
+
+pessoa.descrever();
