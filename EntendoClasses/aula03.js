8dec300604bb5af183ea6cd722c6c3f7530fda3b
André Luiz da Silva <andre.java.ads.2022@gmail.com>
2024-03-23 19:28:54 -0300

    Classes - aula 03

---
+++ EntendoClasses/aula03.js
@@ -0,0 +1,27 @@
+class Pessoa{
+    nome;
+    idade;
+
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+
+    }
+}
+
+function compararPessoas(p1,p2){
+    if(p1.idade > p2.idade){
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`);
+    }
+    else if(p1.idade < p2.idade){
+        console.log(`${p2.nome} é mais velho(a) que ${p1.nome}`);
+    }
+    else{
+        console.log(`${p1.nome} tem a mesma idade que ${p2.nome}`);
+    }
+}
+
+const andre = new Pessoa('André',35);
+const laisa = new Pessoa('Laisa',25);
+
+compararPessoas(andre,laisa);
